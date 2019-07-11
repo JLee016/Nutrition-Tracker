028655f3eb168ced83cc7e280556b1534d7f6ddf
Cole Braswell <braswellcole@icloud.com>
2019-07-11 17:24:12 -0500

    Changed the message to show off branches and such

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Jacob is a hacker man
+          More like Cole is the hacker man!
           
         </p>
         <a
