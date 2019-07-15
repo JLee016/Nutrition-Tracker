6d396318ddf475b2e2179bb12ebfef0d6e65818d
JLee016 <jacobleecode@gmail.com>
2019-07-15 18:29:25 -0500

    Played around with divs

--- src/App.js
+++ src/App.js
@@ -34,22 +34,24 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        
-      <header className="App-Title">
-      <h1>Whats the Nuts?</h1>
-      </header>
-      <header className="App-Search">
+      <div className="App-Background">
+        <div className="row">
+          <div className="col">
+            Whats the nuts?
+          </div>
+        </div>
+        <div className="row App-Search">
+          <ReactSearchBox 
+             placeholder="Tacos"
+              value="Doe"
+              data={this.data}
+              callback={record => console.log(record)}
+              inputBoxFontColor="black"
+              inputBoxFontSize="10"
+  />
+          </div>  
+
       
-      <ReactSearchBox 
-         placeholder="Tacos"
-          value="Doe"
-          data={this.data}
-         callback={record => console.log(record)}
-          inputBoxFontColor="black"
-          inputBoxFontSize="10"
-        />
-      </header>
       
     </div>
     );
